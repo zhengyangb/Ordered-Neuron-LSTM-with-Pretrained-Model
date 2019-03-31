9fd93616d6ab984e0f8d052bbf0ab897339380ae
yvonneyunanhu <yh1844@nyu.edu>
2019-03-31 19:57:36 -0400

    fix plot bug

--- OrderedNeurons/test_phrase_grammar.py
+++ OrderedNeurons/test_phrase_grammar.py
@@ -2,8 +2,9 @@ import argparse
 import re
 import os
 
-import matplotlib.pyplot as plt
+import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import nltk
 import numpy as np
 import torch
